37c9701709f3201a007f75165382aa3f99de31ed
dimas bagas <128117294+Dimas-Bhagaskoro@users.noreply.github.com>
2023-07-05 04:46:59 +0700

    Add files via upload

---
+++ tugas 4/pemangkatan/main.cpp
@@ -0,0 +1,21 @@
+    #include <iostream>
+using namespace std;
+
+int pangkatrec(int a, int b);
+
+int main(){
+    int a,b;
+    cout << "masukan bilangan yg dipangkatkan: ";
+    cin >> a;
+    cout << "pangkat: ";
+    cin >> b;
+    cout << "hasil "  << a << " pangkat " << b << " : "  << pangkatrec(a,b);
+    return 0;
+}
+
+int pangkatrec(int a, int b){
+    if (b==0)
+        return 1;
+    else
+        return a* pangkatrec(a,b-1);
+}
